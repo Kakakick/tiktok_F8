04248e0ab3e5142b502171051d672cc3fbf95fda
tuan <anhlagaumeo203@gmail.com>
2023-08-28 22:05:44 +0700

    useLayoutEffect

--- src/Content.js
+++ src/Content.js
@@ -1,72 +1,38 @@
 
-import { useEffect, useState } from "react"
+import { useEffect, useState, useLayoutEffect } from "react"
 
 
-// 1. useEffect(callback)
-// - Gọi callback mỗi lần khi component re-render
-// - Gọi callback sau khi component thêm element vào DOM
+// useEffect
+// 1. Cập nhật lại state
+// 2. Cập nhật lại Dom (mutated)
+// 3. Render lại UI
+// 4. Gọi clean up nếu deps thay đổi
+// 4. Gọi useEffect callback
 
-// 2. useEffect(callback, [])
-// - Gọi callback đúng 1 lần sau khi component mounted
-
-// 3. useEffect(callback, [deps])
-// - Gọi callback sau mỗi lần deps thay đổi
-
-//--------
-// => tất cả callback đều đc gọi sau khi component mounted lần đầu tiên
-// => Cleanup function luôn đc gọi trước khi component unmouted
-// => Cleanup function luôn đc gọi trước khi callback được gọi (trừ lần mounted)
-
-const lessons = [
-    {
-        id: 1,
-        name: 'NodeJS'
-    },
-    {
-        id: 2,
-        name: 'ReactJS'
-    },
-    {
-        id: 3,
-        name: 'MongoDB'
-    }
-]
-
+// useLayoutEffect
+// 1. Cập nhật lại state
+// 2. Cập nhật lại Dom (mutated)
+// 3. Gọi cleanup nếu deps thay đổi (sync)
+// 4. Gọi useLayoutEffect callback (sync)
+// 5. Render lại UI
 function Content() {
-    const [lessonId, setLessonId] = useState(1);
+    const [count, setCount] = useState(0);
 
-    useEffect(() => {
+    console.log(count);
 
-        const handleComment = ({ detail }) => {
-            console.log(detail);
-        }
+    useLayoutEffect(() => {
+        if (count === 4)
+            setCount(0);
+    }, [count])
 
-        window.addEventListener(`lesson-${lessonId}`, handleComment);
-        
-        return () => (
-            window.removeEventListener(`lesson-${lessonId}`, handleComment)
-        )
-    }, [lessonId])
+    const handleRun = () => {
+        setCount(count + 1);
+    }
 
     return (
         <div>
-            {
-                lessons.map(lesson => (
-                    <li
-                        key={lesson.id}
-                        style={
-                            {
-                                color: lessonId === lesson.id ?
-                                    'red' :
-                                    '#333'
-                            }
-                        }
-                        onClick={() => setLessonId(lesson.id)}
-                    >
-                        {lesson.name}
-                    </li>
-                ))
-            }
+            <h1>{count}</h1>
+            <button onClick={handleRun}>Run</button>
         </div>
     )
 }
