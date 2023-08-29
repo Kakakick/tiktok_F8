f4d507845575087d4245b6b939fae5f24b2f60a1
tuan <anhlagaumeo203@gmail.com>
2023-08-29 15:30:31 +0700

    memo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 
-import { useState } from "react";
+import { useState, memo } from "react";
 import Content from './Content';
 
 function App() {
-  const [show, setShow] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount(pre => pre + 1);
+  }
+  const [count2, setCount2] = useState(0);
+
+  const increase2 = () => {
+    setCount2(pre => pre + 1);
+  }
 
   return (
-    <div className="App" style={{ padding: '35px' }}>
-      <button onClick={() => setShow(!show)}>Toggle</button>
-      {show && <Content />}
-
+    <div style={{ padding: '50px' }}>
+      <Content
+        count={count}
+      />
+      <h1 >{count}</h1>
+      <h1 >{count2}</h1>
+      <button onClick={increase}>Click me!</button>    
+      <button onClick={increase2}>Click me 2!</button>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/Content.js
+++ src/Content.js
@@ -1,40 +1,12 @@
 
-import { useEffect, useState, useLayoutEffect } from "react"
+import { useEffect, useState, useLayoutEffect, useRef, memo } from "react"
 
 
-// useEffect
-// 1. Cập nhật lại state
-// 2. Cập nhật lại Dom (mutated)
-// 3. Render lại UI
-// 4. Gọi clean up nếu deps thay đổi
-// 4. Gọi useEffect callback
-
-// useLayoutEffect
-// 1. Cập nhật lại state
-// 2. Cập nhật lại Dom (mutated)
-// 3. Gọi cleanup nếu deps thay đổi (sync)
-// 4. Gọi useLayoutEffect callback (sync)
-// 5. Render lại UI
-function Content() {
-    const [count, setCount] = useState(0);
-
-    console.log(count);
-
-    useLayoutEffect(() => {
-        if (count === 4)
-            setCount(0);
-    }, [count])
-
-    const handleRun = () => {
-        setCount(count + 1);
-    }
-
+function Content({count}) {
+    console.log('render');
     return (
-        <div>
-            <h1>{count}</h1>
-            <button onClick={handleRun}>Run</button>
-        </div>
+        <h1>Hello World {count}</h1>
     )
 }
 
-export default Content;
+export default memo(Content);
